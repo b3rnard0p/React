fcba65824c18fdb82369df9ef9e3a1ef9727b150
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2025-07-03 17:28:47 -0300

    Delete Projetos/Restaurante directory

--- Projetos/Restaurante/Backend-master/src/main/java/org/example/backend/repository/ProdutoPorPedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.repository;
-import org.example.backend.model.ProdutoPorPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoPorPedidoRepository extends JpaRepository<ProdutoPorPedido, Long> {
-}
--- Projetos/Restaurante/Backend-master/src/main/java/org/example/backend/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.repository;
-
-import org.example.backend.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
